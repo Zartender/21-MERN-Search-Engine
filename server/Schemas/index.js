36899ff44ea71a3f76b4bd5503ea663610aa753d
Ian Greenblott <imgreenblott@gmail.com>
2021-12-14 22:40:11 -0500

    : Export your typeDefs and resolvers.

--- server/Schemas/index.js
+++ server/Schemas/index.js
@@ -1,4 +1,4 @@
 const typeDefs =  require('./typeDefs');
 const resolvers =  require('./resolvers');
-
+// type defs , resolvers , added to index.js yesterday
 module.exports = {typeDefs, resolvers};
