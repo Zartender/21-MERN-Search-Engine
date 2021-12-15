93b0f3033b66450b640fb6bd83d722431aecba49
Ian Greenblott <imgreenblott@gmail.com>
2021-12-15 01:36:41 -0500

    Rolled back Searchbooks to a previous iteraiton bevcause I was having difficulty with the versions of apollo/react-hooks and apollo-boost.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,13 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-
-import { searchGoogleBooks } from '../utils/API';
+import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
-// Apollo hook
-import { useMutation } from '@apollo/react-hooks';
-import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -19,7 +14,6 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  const [saveBook, {err} ] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -35,8 +29,6 @@ const SearchBooks = () => {
     }
 
     try {
-// possible problem
-      
       const response = await searchGoogleBooks(searchInput);
 
       if (!response.ok) {
